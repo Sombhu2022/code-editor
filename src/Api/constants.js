ce5f73dd277da36ea1ad17d292f13ec5feb6d752
Sombhu2022 <sombhudas93@gmail.com>
2024-03-16 20:38:47 +0530

    loading add

--- src/Api/constants.js
+++ src/Api/constants.js
@@ -3,7 +3,10 @@ export const LANGUAGE_VERSIONS = [
   { name:"python", version:"3.10.0" },
   { name:"java", version:"15.0.2" },
   { name:"php", version:"8.2.3" },
-  { name:"c", version:"6.3.0" },
+  { name:"csharp", version:"6.12.0" },
+  { name:"typescript" , version:"5.0.3"},
+  // { name: "c++", version: "20" },  // Latest C++ standard (C++20)
+  // { name: "c", version: "latest" }, // Aim for latest C standard
 ];
 
 export const CODE_SNIPPETS = {
@@ -14,5 +17,21 @@ export const CODE_SNIPPETS = {
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Alex';\necho $name;\n",
-  c :"//c++"
+  c: `// Simple C program to print "Hello World"
+  #include <stdio.h>
+
+  int main() {
+      printf("Hello World\n");
+      return 0;
+  }
+  `,
+  cpp: `// Simple C++ program to print "Hello World" with comments
+  #include <iostream>
+
+  int main() {
+      // Print a message to the console
+      std::cout << "Hello World" << std::endl;
+      return 0;
+  }
+  `,
 };
--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { data } from "autoprefixer";
 import Output from "./Output";
@@ -8,21 +8,24 @@ import { CODE_SNIPPETS, LANGUAGE_VERSIONS } from "../Api/constants";
 
 function CodeEditor() {
      const editorRef = useRef();
-     const [value , setValue]=useState()
-     const [language , setLanguage]=useState(LANGUAGE_VERSIONS[0])
-     const [defaultCode , setDefaultCode]=useState(CODE_SNIPPETS[language.name])
+     const [value , setValue]=useState(CODE_SNIPPETS["javascript"])
+     const [language , setLanguage]=useState("javascript")
+     const [version , setVersion]=useState("18.15.0")
+    //  const [defaultCode , setDefaultCode]=useState(CODE_SNIPPETS[language.name])
 
-     const languageSelect = (index)=>{
-      console.log(index);
-          setLanguage(LANGUAGE_VERSIONS[index])
-          setDefaultCode(CODE_SNIPPETS[language.name])
-          console.log(defaultCode);
+     const languageSelect = (lang , version)=>{
+          console.log(lang);
+          setLanguage(lang)
+          setValue(CODE_SNIPPETS[lang])
+          setVersion(version)
+          console.log("code ", value , language , version);
      }
+     
      const onMount = (editor) => {
       editorRef.current = editor;
       editor.focus();
     };
-
+     
   return (
     <>
   
@@ -41,19 +44,18 @@ function CodeEditor() {
       }}
       // height="85vh"
       // width={"55%"}
-      defaultLanguage="javascript"
-      language={language.name}
-      // defaultValue={defaultCode}
-      defaultValue="\n"
+      // defaultLanguage="javascript"
+      language={language}
+      defaultValue={CODE_SNIPPETS[language]}
       value={value}
       onChange={(val)=> setValue(val)}
       theme="vs-dark"
       onMount={onMount}
-      
     />
+
       </div>
 
-      <Output sourceCode={value} language={language.name} version={language.version}/>
+      <Output sourceCode={value} language={language} version={version}/>
 
       </div>
     </>
